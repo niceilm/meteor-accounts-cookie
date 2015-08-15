1e242cf6164d61b9cbbb7c78d805f24ae481b151
flynn <niceilm@naver.com>
2015-08-15 15:37:39 +0900

    change git url

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'flynn:accounts-cookie',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'A user account for cookie base',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:niceilm/meteor-accounts-cookie.git',
+  git: 'https://github.com/niceilm/meteor-accounts-cookie.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
